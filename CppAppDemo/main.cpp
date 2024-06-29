04e69e65d4b0c3d37f70d56194fec9c0de4bcdad
chenjinming <chenjinming@shining3d.com>
2024-06-29 15:03:42 +0800

    without conan

---
+++ CppAppDemo/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include <libdemo.h>
+
+int main(int argc, char** argv)
+{
+    hello();
+    return 0;
+}
---
+++ CppLibDemo/libdemo.cpp
@@ -0,0 +1,6 @@
+#include "libdemo.h"
+#include <iostream>
+
+void hello(){
+    std::cout<<"Hello Shining3D"<<std::endl;
+}
